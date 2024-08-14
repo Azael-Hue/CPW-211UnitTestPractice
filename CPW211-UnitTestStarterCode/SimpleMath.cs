229d5093f99a01e33ff7176709ace754a2726087
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-08-13 19:44:12 -0700

    Implemented Math functions and added initial 3 test methods

--- CPW211-UnitTestStarterCode/SimpleMath.cs
+++ CPW211-UnitTestStarterCode/SimpleMath.cs
@@ -13,24 +13,60 @@ namespace CPW211_UnitTestStarterCode
     * ***************************************************************************/
     public static class SimpleMath
     {
+        /// <summary>
+        /// This method adds two values passed through and returns the sum of the two
+        /// </summary>
+        /// <param name="a"> The first digit to be added </param>
+        /// <param name="b"> The second digit to be added </param>
+        /// <returns> returns the sum of 'a' and 'b' </returns>
         public static double Add(double a, double b)
         {
-            throw new NotImplementedException();
+            double total = a + b;
+            return total;
         }
 
+        /// <summary>
+        /// This method subtracts the second value passed
+        /// through from the first value passed through
+        /// and returns the difference
+        /// </summary>
+        /// <param name="a"> The value we will be subtracting from </param>
+        /// <param name="b"> the amount we will subtract  </param>
+        /// <returns> returns the difference </returns>
         public static double Subtract(double a, double b)
         {
-            throw new NotImplementedException();
+            double total = a - b;
+            return total;
         }
 
+        /// <summary>
+        /// This method multiplies the two values passed through
+        /// </summary>
+        /// <param name="a"> The value that we will multiply </param>
+        /// <param name="b"> The amount of time we will multiply value 'a' </param>
+        /// <returns> the total value of the operation </returns>
         public static double Multiply(double a, double b)
         {
-            throw new NotImplementedException();
+            double total = a * b;
+            return total;
         }
 
+        /// <summary>
+        /// This method divides the first value passed
+        /// through by the second value
+        /// </summary>
+        /// <param name="a"> The numerator </param>
+        /// <param name="b"> The Denominator </param>
+        /// <returns> the result of the operation </returns>
         public static double Divide(double a, double b)
         {
-            throw new NotImplementedException();
+            if ( b == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero");
+            }
+
+            double total = a / b;
+            return total;
         }
     }
 }
--- CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -18,26 +18,61 @@ namespace CPW211_UnitTestStarterCode.Tests
         [DataRow(0, -0)]
         public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
         {
-            // Use the DataRow values to test the Add method
-            Assert.Fail();
+            // Arrange
+            double expectedTotal = num1 + num2;
+
+            // Act
+            double returnValue = SimpleMath.Add(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expectedTotal, returnValue);
         }
 
         [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        [DataRow(5, 12.5)]
+        [DataRow(0, 100)]
+        [DataRow(-1.5, -10)]
+        [DataRow(8, -10)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double value1, double value2)
         {
-            // Use a few pairs of values to test the Multiply method
-            Assert.Fail();
+            // Arrange
+            double expectedTotal = value1 * value2;
+
+            // Act
+            double returnValue = SimpleMath.Multiply(value1, value2);
+
+            // Assert
+            Assert.AreEqual(expectedTotal, returnValue);
         }
 
         [TestMethod]
-        public void Divide_DenominatorZero_ThrowsArgumentException()
+        [DataRow(5, 0)]
+        [DataRow(0, 0)]
+        public void Divide_DenominatorZero_ThrowsArgumentException( double value1, double value2 )
         {
             // Divide by zero should throw an argument exception with a message
             // "Denominator cannot be zero"
-            Assert.Fail();
+
+            // Assert => Act
+            Assert.ThrowsException<ArgumentException>(() => SimpleMath.Divide(value1, value2));
+            
         }
 
         // TODO: Test Divide method with two valid numbers
+        [TestMethod]
+        [DataRow(5, 10)]
+        [DataRow(10, 99)]
+        [DataRow(-1, -10)]
+        [DataRow(7, 25)]
+        public void Divide_TwoNumbers_ReturnTotal( double value1, double value2 )
+        {
+            // Arrange
+            
+
+            // Act
+
+            // Assert
+        }
 
         // TODO: Test subtract method with two valid numbers
     }
